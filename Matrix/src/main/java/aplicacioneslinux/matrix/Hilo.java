bbb03fa979ffe42538eb9ce85bb1f24daca9e3a6
carlosprivitera <carlosprivitera@yahoo.com.ar>
2023-07-02 21:26:52 -0300

    02-07-23 21:26

--- Matrix/src/main/java/aplicacioneslinux/matrix/Hilo.java
+++ Matrix/src/main/java/aplicacioneslinux/matrix/Hilo.java
@@ -2,16 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package aplicacioneslinux.clientecorreosjpaterminal;
+package aplicacioneslinux.matrix;
 
 /**
  *
  * @author carlos
  */
-public class MainClass {
-    
-    public static void main(String[] args) {
-        System.out.println("Hola");
-    }
+public class Hilo extends Thread {
     
 }
